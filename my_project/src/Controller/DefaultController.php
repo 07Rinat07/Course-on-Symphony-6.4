b705d772c97fec7cc11493961474ede74b2cd879
Rinat Sarmuldin <ura07srr@gmail.com>
2025-12-07 20:56:55 +0500

    Main command into Console---------bin/console

--- my_project/src/Controller/DefaultController.php
+++ my_project/src/Controller/DefaultController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller;
 
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
-    public function index(): Response
+    #[Route('/default/{id}/{page}', name: 'blog_default', requirements: ['id' => '\d+'], defaults: ['id' => 1], methods: ['GET'])]
+    public function index(Request $request, int $id, int $page): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+
+        return $this->render('index.html.twig', ['id'=>$id]);
     }
 }
