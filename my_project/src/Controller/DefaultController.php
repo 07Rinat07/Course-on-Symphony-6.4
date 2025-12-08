e50669123d1d0aa89f5af471ec0a7572ddf0fd5a
Rinat Sarmuldin <ura07srr@gmail.com>
2025-12-08 18:57:58 +0500

    Creating a database migration, creating an Entity and CRUD

--- my_project/src/Controller/DefaultController.php
+++ my_project/src/Controller/DefaultController.php
@@ -3,18 +3,19 @@
 namespace App\Controller;
 
 
+use App\Entity\Blog;
+use App\Repository\BlogRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 final class DefaultController extends AbstractController
 {
-    #[Route('/default/{id}/{page}', name: 'blog_default', requirements: ['id' => '\d+'], defaults: ['id' => 1], methods: ['GET'])]
-    public function index(Request $request, int $id, int $page): Response
+    #[Route('/', name: 'blog_default')]
+    public function index(): Response
     {
-
-        return $this->render('index.html.twig', ['id'=>$id]);
+        return $this->render('index.html.twig', []);
     }
 }
